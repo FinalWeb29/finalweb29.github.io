4d40bbee8a004e7983dd3f807ef600a4ee2612b8
FinalWeb29 <85522600+FinalWeb29@users.noreply.github.com>
2021-06-07 16:47:33 -0500

    Add files via upload

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,12 @@
 'use strict'
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyB6yzdOhe68XxZNUJCO5IJRvPd7s-ZPpvs",
-  authDomain: "webtodo-fdfa9.firebaseapp.com",
-  projectId: "webtodo-fdfa9",
-  storageBucket: "webtodo-fdfa9.appspot.com",
-  messagingSenderId: "1025020512253",
-  appId: "1:1025020512253:web:ec9c445c9d066771137b94"
+  apiKey: "AIzaSyD62ESxYu7yb0pvDMUbG60-9hWrmLuzIp8",
+  authDomain: "finalweb29-a0b62.firebaseapp.com",
+  projectId: "finalweb29-a0b62",
+  storageBucket: "finalweb29-a0b62.appspot.com",
+  messagingSenderId: "538200993230",
+  appId: "1:538200993230:web:bb64511182d47660d54f9f"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
