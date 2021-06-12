f588b76ef8560c28bb6418ce0ead3a640556902d
FinalWeb29 <85522600+FinalWeb29@users.noreply.github.com>
2021-06-11 19:43:24 -0500

    Add files via upload

--- js/ctl.js
+++ js/ctl.js
@@ -5,11 +5,6 @@ var db = firebase.firestore(),
 	month = d.getMonth() + 1,
 	date = d.getDate(),
 	modal = document.getElementById('id01'),
-	boletaC = document.getElementById('boletaC'),
-	nombreC = document.getElementById('nombreC'),
-	carreraC = document.getElementById('carreraC'),
-	fechaC = document.getElementById('fechaC'),
-	doctorC = document.getElementById('doctorC'),
 	modificar = document.getElementById('modificar'),
 	eliminar = document.getElementById('eliminar'),
 	form = document.getElementById('form'),
@@ -71,7 +66,7 @@ async function showModal(cita) {
 	carreraC.innerHTML = carrera;
 	fechaC.innerHTML = fecha;
 	doctorC.innerHTML = doctor;
-	fotoC.src = img;
+	fotoC.src = cod(await bajarArchivo(id));
 	modal.style.display = "flex";
 }
 window.onclick = function(event) {
@@ -84,7 +79,7 @@ function editar(cita) {
 	nombre: form["nombre"].value = nombreC.innerHTML.toString();
 	carrera: form["carrera"].value = carreraC.innerHTML.toString();
 	fecha: form["fecha"].value = fechaC.innerHTML.toString();
-	foto: form["foto"].files[0] = bajarArchivo(id)
+	foto: form["foto"].removeAttribute("required");
 	form["actua"].setAttribute("data-cita", id);
 	form["guardar"].style.display = "none";
 	form["actua"].style.display = "grid";
@@ -118,16 +113,13 @@ async function escribir() {
 async function leer() {
 	db.collection("citas").orderBy("fecha", "asc").onSnapshot((querySnapshot) => {
 		output.innerHTML = "";
-		var img = "";
 	  querySnapshot.forEach((doc) => {
-	  	img = cod(await bajarArchivo(doc.id));
 			output.innerHTML += `<p id="${doc.id}"
 									data-boleta="${doc.data().boleta}"
 									data-nombre="${doc.data().nombre}"
 									data-carrera="${doc.data().carrera}"
 									data-fecha="${doc.data().fecha}"
 									data-doctor="${doc.data().doctor}"
-									data-imgsrc="${img}"
 									onclick="javascript:showModal(this)">
 									Boleta: ${doc.data().boleta} | 
 									Fecha: ${doc.data().fecha}</p>`;
@@ -135,13 +127,12 @@ async function leer() {
 	});
 }
 async function actualizar() {
-	alert("Si en actualizar");
 	var data = {
 		boleta: form["boleta"].value,
-	    nombre: form["nombre"].value,
-	    carrera: form["carrera"].value,
-	    fecha: form["fecha"].value,
-	    doctor: userD.value
+	  nombre: form["nombre"].value,
+	  carrera: form["carrera"].value,
+	  fecha: form["fecha"].value,
+	  doctor: userD.value
 	};
 	db.collection("citas").doc(form["actua"].getAttribute("data-cita")).set(data)
 	.then(() => {
@@ -150,6 +141,7 @@ async function actualizar() {
 	})
 	.catch((error) => {console.error("Error al registrar: ", error);});
 	subirArchivo(form["foto"].files[0], form["actua"].getAttribute("data-cita"));
+
 }
 async function borrar(cita) {
 	if(confirm("¿Quieres eliminar la cita?")) {
